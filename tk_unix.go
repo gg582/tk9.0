9acfc6c7d7f2bbc3568c299bce028f7beb296562
Jan Mercl <0xjnml@gmail.com>
2024-09-18 18:01:11 +0200

    linux/arm64: switch to purego

--- tk_unix.go
+++ tk_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !windows && !(linux && amd64)
+//go:build !windows && !(linux && (amd64 || arm64))
 
 package tk9_0 // import "modernc.org/tk9.0"
 
