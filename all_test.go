5fd8fb5e70ef1cdba578dd37a6448c86dfbddded
Jan Mercl <0xjnml@gmail.com>
2024-09-26 15:53:46 +0200

    windows: fix "no test files"

--- all_test.go
+++ all_test.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !windows
-
 package tk9_0 // import "modernc.org/tk9.0"
 
 import (
